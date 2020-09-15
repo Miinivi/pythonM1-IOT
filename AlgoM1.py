2fbe788eb3e6d24751cc2ec4eefae9970dec8315
Miinivi <45562698+Miinivi@users.noreply.github.com>
2020-09-15 16:59:00 +0200

    Add files via upload

--- AlgoM1.py
+++ AlgoM1.py
@@ -2,9 +2,10 @@
 
 a = 2
 b = 3
-c = a
-a = b
-b = c
+#c = a
+#a = b
+#b = c
+a,b = b,a 
 print("a = ", a) #Check value a
 print("b = ", b) #Check value b
 
@@ -46,3 +47,60 @@ for i in range(nbbDD+1):
     sommeStr += " + " + str(i)
 print(sommeStr)
 print(somme)
+
+##Exo 2.1
+
+liste = [i for i in range(5)]
+sommeListe = 0
+for i in range(len(liste)):
+    sommeListe += liste[i]
+    #print("Liste, valeur de i, i = ", i, " : ", liste[i]) #Check list values
+print(sommeListe) #Or sum(liste)
+
+##Exo 2.2
+
+liste2 = [i for i in range(5)]
+liste3 = [0 for i in range(5)]
+for i in range(len(liste)):
+    liste3[i] = liste[i] + liste2[i]
+print(liste3)
+
+##Exo 2.3
+
+liste4 = [0 for i in range(5)]
+for i in range(len(liste)):
+    liste4[i] = liste[i] * liste2[i]
+print(sum(liste4))
+
+##Exo 2.4
+
+liste5 = [0 for i in range(3)]
+for i in range(3):
+    liste5[i] = int(input())
+
+max = liste5[0]
+for i in range(len(liste5)):
+    if (liste5[i] > max):
+        max = liste5[i]
+print(max)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
