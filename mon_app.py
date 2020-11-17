d498c03fbed6b78ec32710da14e9b67a71fd07e0
Miinivi <45562698+Miinivi@users.noreply.github.com>
2020-11-17 14:30:43 +0100

    Add files via upload

--- mon_app.py
+++ mon_app.py
@@ -16,16 +16,31 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 #### READING DATASET ####
 indianFood = pd.read_csv("indian_food.csv")
-#### RESOLVING ERROR ####
-#### CONCERNED DATASET #### 
-# https://www.kaggle.com/northon/globalterrorismdatabase-compact
-
-#### READING DATASET ####
 food = pd.read_csv("Food_Preference.csv")
-#### CONCERNED DATASET #### 
-# https://www.kaggle.com/abcsds/pokemon/activity
 
 #### DISPLAYING DATA ####
-#pokemon_df = load_data()
-st.title('hello')
-st.write(food.head(5))
+st.title('Mon app en python')
+st.header('Food preferences')
+st.subheader('Dataset')
+st.write(food.head(2)) #or st.table
+st.subheader('Colonnes')
+st.write(food.columns)
+st.subheader('Type des colonnes')
+st.write(food.dtypes)
+st.subheader('Shape')
+st.write(food.shape)
+st.subheader('Info')
+st.write(food.info())
+#
+st.header('Indian food')
+st.subheader('Dataset')
+st.write(indianFood.head(2)) #or st.table
+st.subheader('Colonnes')
+st.write(indianFood.columns)
+st.subheader('Type des colonnes')
+st.write(indianFood.dtypes)
+st.subheader('Shape')
+st.write(indianFood.shape)
+st.subheader('Info')
+st.write(food.info())
+
