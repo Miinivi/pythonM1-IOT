4d56c115154e3c67a351b39024b37d95f10ad9cd
Miinivi <45562698+Miinivi@users.noreply.github.com>
2020-11-17 11:35:57 +0100

    Add files via upload

--- mon_app.py
+++ mon_app.py
@@ -15,7 +15,7 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 #### READING DATASET ####
-indianFood = pd.read_csv("indian_food")
+indianFood = pd.read_csv("indian_food.csv")
 #### RESOLVING ERROR ####
 #### CONCERNED DATASET #### 
 # https://www.kaggle.com/northon/globalterrorismdatabase-compact
@@ -26,11 +26,6 @@ food = pd.read_csv("Food_Preference.csv")
 # https://www.kaggle.com/abcsds/pokemon/activity
 
 #### DISPLAYING DATA ####
-@st.cache
-def load_data():
-   data_path = Path() / 'Food_Preference.csv'
-   data = pd.read_csv(data_path)
-   return data
-
 #pokemon_df = load_data()
+st.title('hello')
 st.write(food.head(5))
